48e65c81b3bc8f9dbedf1e6d108d6fa2ce46a921
vidath96 <vidathhasantha96@gmail.com>
2023-05-31 08:37:16 +0530

    Node Module Update

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const helmet = require('helmet');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var movieRouter = require('./routes/movie');
+var searchRouter = require('./routes/search');
+
+// var usersRouter = require('./routes/users');
 
 var app = express();
+
+app.use(helmet({
+    contentSecurityPolicy: false,
+}));
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -15,6 +23,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/movie', movieRouter);
+app.use('/search', searchRouter);
+// app.use('/users', usersRouter);
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+const movies = require('../data/movies')
+// const person = require('../data/people')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.json({ title: 'Express' });
+});
+
+router.get('/most-popular', function(req, res, next) {
+  if(req.query.api_key != 123456789){
+      res.json({
+        msg: 'Invalid API Key'
+      })
+  }else{
+    const results = movies.filter((movie) => {
+      return movie.most_popular
+    })
+    res.json(results)
+  }
 });
 
 module.exports = router;
